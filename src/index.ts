427343a295cd210ece085445577e80fa923434e7
creativoma <ma.marianoalvarez@gmail.com>
2026-02-14 01:56:17 +0100

    perf: remove demo CSS from library bundle (-13.5% size reduction)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import './index.css'
-
 export { default as LiquidGlass } from './components/LiquidGlass'
 export type { LiquidGlassProps } from './components/types'
 export { useBrowserDetection } from './hooks/useBrowserDetection'
